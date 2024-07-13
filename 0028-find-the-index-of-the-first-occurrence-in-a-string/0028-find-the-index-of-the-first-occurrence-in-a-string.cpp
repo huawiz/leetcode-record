9f546d73a86abdc396ec0dee6b2f432bf4d88793
Justin Wu <90091107+huawiz@users.noreply.github.com>
2024-07-14 00:47:48 +0800

    Time: 0 ms (100%), Space: 8 MB (14.49%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    void getNext(int* next, const string& s) {
-        int j = -1;
+    void getNext(int* next,const string& s){
+        int j = 0;
         next[0] = j;
-        for(int i = 1; i < s.size(); i++) { // 注意i从1开始
-            while (j >= 0 && s[i] != s[j + 1]) { // 前后缀不相同了
-                j = next[j]; // 向前回退
+        for(int i =1;i<s.size();i++){
+            while(j>0&&s[i]!=s[j]){
+                j = next[j-1];
             }
-            if (s[i] == s[j + 1]) { // 找到相同的前后缀
+            if(s[i]==s[j]){
                 j++;
             }
-            next[i] = j; // 将j（前缀的长度）赋给next[i]
+            next[i] = j;
         }
     }
     int strStr(string haystack, string needle) {
-        if (needle.size() == 0) {
-            return 0;
-        }
-		vector<int> next(needle.size());
-		getNext(&next[0], needle);
-        int j = -1; // // 因为next数组里记录的起始位置为-1
-        for (int i = 0; i < haystack.size(); i++) { // 注意i就从0开始
-            while(j >= 0 && haystack[i] != needle[j + 1]) { // 不匹配
-                j = next[j]; // j 寻找之前匹配的位置
+        if (needle.size() ==0){return 0;}
+        vector<int> next(needle.size());
+        getNext(&next[0],needle);
+        int j = 0;
+        for(int i=0;i<haystack.size();i++){
+            while(j>0&&haystack[i]!=needle[j]){
+                j = next[j-1];
             }
-            if (haystack[i] == needle[j + 1]) { // 匹配，j和i同时向后移动
-                j++; // i的增加在for循环里
+            if(haystack[i]==needle[j]){
+                j++;
             }
-            if (j == (needle.size() - 1) ) { // 文本串s里出现了模式串t
-                return (i - needle.size() + 1);
+            if(j==needle.size()){
+                return (i-needle.size()+1);
             }
         }
         return -1;
